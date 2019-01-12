0824b44028fdf54df7c4bc0e20b09da9173cae8e
Gregory Cardoz <gregorycardoz@Gregorys-MacBook-Pro-3.local>
2019-01-12 12:53:32 +1100

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value1: '', value2: '', value3: ''};
+
+    this.handleChange1 = this.handleChange1.bind(this);
+    this.handleChange2 = this.handleChange2.bind(this);
+    this.handleChange3 = this.handleChange3.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange1(event) {
+    this.setState({value1: event.target.value});
+  }
+
+    handleChange2(event) {
+        this.setState({value2: event.target.value});
+      }
+
+    handleChange3(event) {
+
+      this.state.value3 = this.state.value1;
+      //alert('A name was submitted: ' + this.state.value3);
+      var h = +this.state.value1 + +this.state.value2;
+      this.setState({value3: h});
+
+
+      }
+
+  handleSubmit(event) {
+  //  alert('A name was submitted: ' + this.state.value1);
+  //  alert('A age was submitted: ' + this.state.value2);
+    this.state.value3 = this.state.value1;
+    event.preventDefault();
+  }
+
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <form onSubmit={this.handleSubmit}>
+        <label>
+          Number 1 :
+          <input   type="text" value={this.state.value1} onChange={this.handleChange1} />
+        </label>
+          <br />
+        <label>
+          Number 2 :
+          <input  type="text" value={this.state.value2} onChange={this.handleChange2} />
+        </label>
+          <br />
+        <label>
+          Total :
+          <input  type="text"  value={this.state.value3}  />
+        </label>
+        <br />
+        {/*
+
+         sfdsfsdf sfsfsasddsada
+
+        <label>
+          Age:
+          <input type="text" value={this.state.value2} onChange={this.handleChange2} />
+        </label>
+
+        */}
+
+          <input type="button" value="Welcome" onClick={this.handleChange3} />
+
+            <br />
+            <input type="submit" value="Submit" />
+      </form>
+
         </header>
       </div>
     );
